50a40cf0b248f6c71d9c3699b68f156e98877388
Oliver van den Bosch <74139069+ovandenbosch@users.noreply.github.com>
2022-03-03 10:19:12 -0500

    Add time remaingin

--- rlclock.1m.js
+++ rlclock.1m.js
@@ -1,8 +1,5 @@
-#!/usr/bin/env /usr/local/bin/node
+#!/usr/local/bin/node
 
-
-// Metadata allows your plugin to show up in the app, and website.
-//
 //  <xbar.title>RL Clock</xbar.title>
 //  <xbar.version>v1.0</xbar.version>
 //  <xbar.author>Oliver van den Bosch</xbar.author>
@@ -10,14 +7,12 @@
 //  <xbar.desc>An xbar plugin that provides a quick look at information about the Roxbury Latin school day.</xbar.desc>
 //  <xbar.image>https://upload.wikimedia.org/wikipedia/en/6/66/Roxbury_Latin_School_coa.png</xbar.image>
 //  <xbar.dependencies>node</xbar.dependencies>
-//  
-
-
 
 import fetch from "node-fetch";
-
-const getInfo = () => {
+function getInfo() {
   let blockInfoUrl = "https://mod-clock-api.roxburylatin.org/daytype.json";
+  let scheduleUrl =
+    "https://mod-clock-api.roxburylatin.org/todays_schedule.json";
 
   fetch(blockInfoUrl)
     .then((res) => {
@@ -26,14 +21,36 @@ const getInfo = () => {
     .then((data) => {
       let dayType = data["dayType"];
       let hallLength = data["hallLength"];
-      let currentBlock = data["currentBlock"]["name"];
-
-      console.log(`Day Type | ${dayType}`);
+      let currentBlock = data["currentBlock"]["block"];
+      let remainingMin = data["remainingMin"];
+      console.log(`${currentBlock} Block - ${remainingMin} minutes remaining`);
+      console.log("---");
       if (hallLength > 0) {
-        console.log(`Hall Length | ${hallLength}`);
+        console.log(`Hall Length ${hallLength}`);
       }
-      console.log(`Current Block | ${currentBlock}`);
+      console.log(`${dayType} Day`);
     });
-};
+
+  fetch(scheduleUrl)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log("---")
+      let periods = data["periods"]
+      periods.forEach(period => {
+        let pName = period["name"]
+        let block = period["block"]
+        if (block == undefined) {
+          block = "";
+        } else {
+          block += " Block"
+        }
+        let start = period["start"]
+        let end = period["end"]
+        console.log(`${pName} - ${block} ${start} - ${end}`)
+      });
+    });
+}
 
 getInfo();
